168cb8abb1ad97f33a8429a659c8dea0b915dba9
River Chen <yanherchen@gmail.com>
2017-07-11 15:53:55 +0800

    Add files via upload

---
+++ WebSiteManager.java
@@ -0,0 +1,7 @@
+package js_parser;
+
+public class WebSiteManager {
+	public static String getWebSite(){
+		return "https://www.kingnet.com.tw/knNew/inquiry/consult.html?inquiryId="+PageManager.getpage();
+	}
+}
